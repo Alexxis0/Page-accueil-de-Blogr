efe12bd1207c7574f939633c4fba15045771f598
Alexis Morantin <alexismorantin44390@gmail.com>
2023-01-13 20:07:29 +0100

    menu mobile

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ let body = document.querySelector("body");
 let titleNav = document.querySelectorAll(".title_nav");
 let arrowBottom = document.querySelectorAll(".arrow_bottom");
 let activeMenu = document.querySelectorAll(".active_menu");
+let iconHamburger = document.querySelector(".icon_hamburger");
+let iconHamburgerClose = document.querySelector(".icon_hamburger_close");
 
 linkMenu.forEach((element) => {
     element.addEventListener("click", () => {
@@ -11,4 +13,13 @@ linkMenu.forEach((element) => {
         element.children[1].classList.toggle("arrow_bottom_rotate");
         element.parentElement.children[1].classList.toggle("active_menu");
     });
+});
+
+iconHamburger.addEventListener("click", () => {
+    iconHamburgerClose.classList.toggle("ouvrir");
+    iconHamburger.classList.toggle("fermer");
+});
+iconHamburgerClose.addEventListener("click", () => {
+    iconHamburgerClose.classList.toggle("ouvrir");
+    iconHamburger.classList.toggle("fermer");
 });
